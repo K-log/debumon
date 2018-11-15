19761b6fb58942e7da36fe3f388bdda881e8709a
ironsketch <null@nullface.net>
2018-11-14 21:23:37 -0800

    starting to create the card files, commiting incase K-log wants to view

---
+++ card.cpp
@@ -0,0 +1,36 @@
+#include "card.h"
+
+Card::Card(int new_damage, int new_cost){
+    damage = new_damage;
+    cost = new_cost;
+}
+int Card::getDamage(){
+    return damage;
+}
+int Card::getCost(){
+    return cost;
+}
+void Card::changeX(int new_x){
+    card.x = new_x;
+}
+void Card::changeY(int new_Y){
+    card.y = new_Y;
+}
+int Card::getX(){
+    return card.x;
+}
+int Card::getY(){
+    return card.y;
+}
+void Card::changeWidth(int new_Width){
+    card.w = new_Width;
+}
+void Card::changeHeight(int new_Height){
+    card.h = new_Height;
+}
+int Card::getWidth(){
+    return card.w;
+}
+int Card::getHeight(){
+    return card.h;
+}
---
+++ card.h
@@ -0,0 +1,22 @@
+#ifndef CARD_H
+#define CARD_H
+#include "SDL2/SDL.h"
+
+class Card{
+    public:
+        Card(int new_damage, int new_cost);
+        int getDamage();
+        int getCost();
+        void changeX(int new_x);
+        void changeY(int new_y);
+        int getX();
+        int getY();
+        void changeWidth(int new_Width);
+        void changeHeight(int new_Height);
+        int getWidth();
+        int getHeight();
+    private:
+        int damage, cost;
+        SDL_Rect card;
+};
+#endif
