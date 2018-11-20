0a1824a5eb41236a3ed43b843c2db1fcd143842f
k-log <noahb343@gmail.com>
2018-11-19 20:02:47 -0800

    Cleaned up working directory

--- SDL2-2.0.9/build/SDL_stdlib.d
+++
@@ -1,9 +0,0 @@
-build/SDL_stdlib.lo: \
- /home/k-log/debumon/SDL2-2.0.9/src/stdlib/SDL_stdlib.c \
- /home/k-log/debumon/SDL2-2.0.9/src/stdlib/../SDL_internal.h \
- /home/k-log/debumon/SDL2-2.0.9/src/stdlib/../dynapi/SDL_dynapi.h \
- /home/k-log/debumon/SDL2-2.0.9/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_stdinc.h include/SDL_config.h \
- /home/k-log/debumon/SDL2-2.0.9/src/stdlib/../libm/math_libm.h \
- /home/k-log/debumon/SDL2-2.0.9/src/stdlib/../libm/../SDL_internal.h
